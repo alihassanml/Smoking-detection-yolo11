bbef478fad3d6d18e4a18bf02fadce1a09f55e15
alihassanml <fa-22-bscs-294@lgu.edu.pk>
2024-11-06 20:03:49 +0500

    smoking detection

--- app.py
+++ app.py
@@ -2,22 +2,17 @@ from ultralytics import YOLO
 import cv2
 
 model = YOLO('best.onnx')
-cap = cv2.VideoCapture(0)
+image = 'c.jpg'
 
 while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Failed to grab frame.")
-        break  
 
-    results = model(frame)
+    results = model(image)
     result = results[0]
 
     annotated_frame = result.plot() 
-    cv2.imshow('YOLO Inference', annotated_frame)
+    cv2.imwrite('output2.jpg',annotated_frame)
     
     if cv2.waitKey(1) == 27:
         break
 
-cap.release()
 cv2.destroyAllWindows()
